5d492ff8dcb1557b98e4325ce173290d81312fa2
artsiom90 <artsiom.trush@gmail.com>
2022-08-29 21:34:52 +0300

    feat: add axios

--- src/api/API.ts
+++ src/api/API.ts
@@ -1,8 +1,10 @@
+import axios from "axios"
+
 export class API {
-    static async fetchData(category: number, sortBy: string, searchVale: string) {
+    static async fetchData(category: number, sortBy: string, searchValue: string) {
         try {
-            const response = await fetch(`http://localhost:3001/meals?&category=${category}&q=${searchVale}&_sort=${sortBy}&_order=asc`)
-            const data = await response.json()
+            const { data } = await axios
+                .get(`http://localhost:3001/meals?&category=${category}&q=${searchValue}&_sort=${sortBy}&_order=asc`)
             return data
         } catch (error) {
             console.error(error)
